5ed9d3860f0c8a6d102496b2416ffc24656df80f
guilherme <devguilhermeribeiro000@gmail.com>
2025-03-24 22:15:36 -0300

    Adding the config for database connection

---
+++ src/Config/Database.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Config;
+
+use mysqli;
+
+class Database
+{
+    private $_host;
+    private $_dbname;
+    private $_user;
+    private $_password;
+    private $_conn;
+
+    public function __construct()
+    {
+        $dotenv = file(__DIR__ . '/../../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $env = [];
+
+        foreach ($dotenv as $line) {
+            if (strpos($line, '#') === 0) {
+                continue;
+            }
+            list($key, $value) = explode('=', $line, 2);
+            $env[$key] = $value;
+            putenv("$key=$value");
+        }
+
+        $this->_host = getenv('DB_HOST');
+        $this->_dbname = getenv('DB_NAME');
+        $this->_user = getenv('DB_USER');
+        $this->_password = getenv('DB_PASSWORD');
+    }
+
+
+    public function connect()
+    {
+        try {
+            $this->_conn = new mysqli($this->_host, $this->_user, $this->_password, $this->_dbname);
+
+            if ($this->_conn->connect_error) {
+                throw new \Exception("Erro ao conectar ao banco de dados" . $this->_conn->connect_error);
+            }
+
+            echo "Conectado ao banco de dados com sucesso!";
+            return $this->_conn;
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
+}
