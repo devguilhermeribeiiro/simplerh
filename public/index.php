b50a65a0b5641b43715f8376b8fb91e065374eef
guilherme <devguilhermeribeiro000@gmail.com>
2025-03-28 19:32:48 -0300

    Changing folder's name

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\Config\Database;
 use App\Config\Router;
 
-require __DIR__ . '/../src/web/Routes.php';
+require __DIR__ . '/../src/Web/Routes.php';
 
 $db = new Database();
 $db->connect();
